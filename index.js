b56134fe24ca09d1db28ad30aafefc25779473f1
ryuji <forest.book.programmer@gmail.com>
2023-05-18 07:33:40 +0900

    脆弱性のアプリほぼ完成

--- index.js
+++ index.js
@@ -1,7 +1,88 @@
 import express from "express";
+import { readFileSync } from "fs";
+import serveStatic from "serve-static";
+import { TechGeekDB } from "./techgeek-db.js";
+import cookieParser from "cookie-parser";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const PORT = 3000;
+const FRONT_END_PATH = "./frontend";
+TechGeekDB.init();
+// ミドルウェアの定義
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/post.html", (req, res, next) => {
+  // ログインチェック
+  console.log("url", req.url);
+  const token = req.cookies.session_key;
+  if (!token) {
+    return res.redirect("/login.html");
+  }
+  try {
+    const user = jwt.verify(token, "techgeek");
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.redirect("/login.html");
+  }
+});
+
+app.post("/api/signup", async (req, res) => {
+  const { name, email, phone, password, confirm_password } = req.body;
+  console.log(req.body);
+  const hasedPassword = await bcryptjs.hash(password, 10);
+  const user = await TechGeekDB.createUser(name, phone, email, hasedPassword);
+  const token = jwt.sign(user, "techgeek", { expiresIn: '1d' });
+  res.cookie("session_key", token, { maxAge: 1000 * 60 * 60 * 24 });
+  return res.redirect("/post.html");
+});
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  const user = await TechGeekDB.getUser(email);
+  if (!user) {
+    console.log("ユーザーが見つかりません");
+    return res.redirect("/login.html");
+  }
+  const isValidPassword = await bcryptjs.compare(password, user.password);
+  if (!isValidPassword) {
+    console.log("パスワードが間違っています");
+    return res.redirect("/login.html");
+  }
+
+  const token = jwt.sign(user, "techgeek", { expiresIn: '1d' });
+  res.cookie("session_key", token, { maxAge: 1000 * 60 * 60 * 24 });
+  return res.redirect("/post.html");
+});
+
+app.post("/api/post", async (req, res) => {
+  const { content, session, category } = req.body;
+  console.log(req.body);
+  // sessionからユーザー情報を取得
+  const user = jwt.verify(session, "techgeek");
+  console.log(user);
+  const post = await TechGeekDB.createPost(category, content, user.name, user.phone, user.email);
+  console.log({ post });
+  return res.redirect("/post.html");
+});
+app.get("/api/posts", async (req, res) => {
+  const posts = await TechGeekDB.getPosts();
+  return res.status(200).json(posts);
+});
+
+
+app.use(serveStatic(FRONT_END_PATH, { index: false }));
+
+app.get("/*", (req, res) => {
+  return res
+    .status(200)
+    .set("Content-Type", "text/html")
+    .send(readFileSync(`${FRONT_END_PATH}/index.html`));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
---
+++ techgeek-db.js
@@ -0,0 +1,156 @@
+// DBは、PostgreSQLを採用
+// postgres://ユーザー名:パスワード@localhost:port/DB名
+import pg from 'pg';
+export const TechGeekDB = {
+  init: async function () {
+    const hasUsersTable = await this.__hasUsersTable();
+    console.log(hasUsersTable);
+    if (hasUsersTable) {
+      this.ready = Promise.resolve();
+    } else {
+      const query = `
+        CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) NOT NULL,
+          phone VARCHAR(255) NOT NULL,
+          email VARCHAR(255) NOT NULL,
+          password VARCHAR(255) NOT NULL,
+          createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        )
+      `;
+      this.ready = this.__query(query);
+    }
+
+    const hasPostsTable = await this.__hasPostsTable();
+
+    if (hasPostsTable) {
+      this.ready = Promise.resolve();
+    } else {
+      const query = `
+        CREATE TABLE posts (
+          id SERIAL PRIMARY KEY,
+          user_name VARCHAR(255) NOT NULL,
+          user_email VARCHAR(255) NOT NULL,
+          user_phone VARCHAR(255) NOT NULL,
+          content VARCHAR(255) NOT NULL,
+          category VARCHAR(255) NOT NULL,
+          createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        )
+      `;
+      this.ready = this.__query(query);
+    }
+
+  },
+  __hasUsersTable: async function () {
+    const query = `
+      SELECT EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_name = 'users'
+      )
+    `;
+    const result = await this.__query(query);
+    return result.rows[0].exists;
+  },
+  __hasPostsTable: async function () {
+    const query = `
+      SELECT EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_name = 'posts'
+      )
+    `;
+    const result = await this.__query(query);
+    return result.rows[0].exists;
+  },
+  __query: function (sql, params = []) {
+    const pool = new pg.Pool({
+      connectionString: "postgres://morimotoryuuji:@localhost:5432/security",     //process.env.DATABASE_URL,
+      ssl: false //process.env.NODE_ENV == "development" ? false : { rejectUnauthorized: false }
+    });
+    return new Promise((resolve, reject) => {
+      pool.query(sql, params, (err, result) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(result);
+      });
+    });
+  },
+  createUser: async function (name, phone, email, password) {
+    await this.ready;
+    const query = `
+      INSERT INTO users (name, phone, email, password)
+      VALUES ($1, $2, $3, $4)
+      RETURNING id, name, phone, email, createdAt
+    `;
+    const result = await this.__query(query, [name, phone, email, password]);
+    console.log(result.rows[0]);
+    return result.rows[0];
+  },
+  getUser: async function (email) {
+    await this.ready;
+    const query = `
+      SELECT * FROM users
+      WHERE email = $1
+    `;
+    const result = await this.__query(query, [email]);
+    return result.rows[0];
+  },
+  getUserById: async function (id) {
+    await this.ready;
+    const query = `
+      SELECT * FROM users
+      WHERE id = $1
+    `;
+    const result = await this.__query(query, [id]);
+    return result.rows[0];
+  },
+  createPost: async function (category, content, user_name, user_phone, user_email) {
+    await this.ready;
+    const query = `
+      INSERT INTO posts (category, content, user_name, user_phone, user_email)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING id, category, content, user_name, user_phone, user_email, createdAt
+    `;
+    const result = await this.__query(query, [category, content, user_name, user_phone, user_email]);
+    return result.rows[0];
+  },
+  getPosts: async function () {
+    await this.ready;
+    const query = `
+      SELECT * FROM posts
+    `;
+    const result = await this.__query(query);
+    return result.rows;
+  },
+  getPost: async function (id) {
+    await this.ready;
+    const query = `
+      SELECT * FROM posts
+      WHERE id = $1
+    `;
+    const result = await this.__query(query, [id]);
+    return result.rows[0];
+  },
+  updatePost: async function (id, category, content, user_name, user_phone, user_email) {
+    await this.ready;
+    const query = `
+      UPDATE posts
+      SET category = $2, content = $3, user_name = $4, user_phone = $5, user_email = $6
+      WHERE id = $1
+      RETURNING id, category, content, user_name, user_phone, user_email, createdAt
+    `;
+    const result = await this.__query(query, [id, category, content, user_name, user_phone, user_email]);
+    return result.rows[0];
+  },
+  deletePost: async function (id) {
+    await this.ready;
+    const query = `
+      DELETE FROM posts
+      WHERE id = $1
+    `;
+    await this.__query(query, [id]);
+  }
+};
